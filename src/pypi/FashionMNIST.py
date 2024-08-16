170bf575ff4b5a4cb1bebc3f2129e7a3509297e1
jaysinco <jaysinco@163.com>
2024-08-16 13:46:52 +0800

    add fashion mnist test

--- src/pypi/FashionMNIST.py
+++ src/pypi/FashionMNIST.py
@@ -15,7 +15,6 @@ class ClassifyNet(nn.Module):
             nn.Linear(512, 512),
             nn.ReLU(),
             nn.Linear(512, 10),
-            nn.Softmax(dim=1)
         )
 
     def forward(self, x):
@@ -41,13 +40,49 @@ def train(learning_rate, batch_size, epochs):
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-    for X, y in train_dataloader:
-        print(X.shape, y.shape)
-        break
+    for t in range(epochs):
+        print(f"Epoch {t+1}/{epochs}\n-------------------------------")
+        train_loop(train_dataloader, model, loss_fn, optimizer)
+        test_loop(test_dataloader, model, loss_fn)
+    print("Done!")
+
+
+def train_loop(dataloader: DataLoader, model: nn.Module, loss_fn, optimizer: torch.optim.Optimizer):
+    model.train()
+    size = len(dataloader.dataset)
+
+    for batch, (X, y) in enumerate(dataloader):
+        pred: torch.Tensor = model(X)
+        loss: torch.Tensor = loss_fn(pred, y)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * batch_size + len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+
+def test_loop(dataloader: DataLoader, model: nn.Module, loss_fn):
+    model.eval()
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred: torch.Tensor = model(X)
+            loss: torch.Tensor = loss_fn(pred, y)
+            test_loss += loss.item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {
+          (100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 
 if __name__ == "__main__":
     learning_rate = 1e-3
     batch_size = 64
-    epochs = 5
+    epochs = 30
     train(learning_rate, batch_size, epochs)
