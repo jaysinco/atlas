9a46a9baf3ae315a0d39b6790d48254fa1df56db
jaysinco <jaysinco@163.com>
2024-12-23 11:46:03 +0800

    add layout route

--- src/flt/lib/main.dart
+++ src/flt/lib/main.dart
@@ -63,10 +63,36 @@ class _ScreenState extends State<Screen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: widgets,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [...widgets],
+            ),
+            Container(
+              height: 20,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 5),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, "test_input");
+                },
+                child: Text("test_input"),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 5),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, "test_layout");
+                },
+                child: Text("test_layout"),
+              ),
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -83,10 +109,104 @@ class _ScreenState extends State<Screen> {
   }
 }
 
+class TestLayoutRoute extends StatelessWidget {
+  const TestLayoutRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Flex(
+          direction: Axis.horizontal,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                height: 30,
+                color: Colors.red,
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                height: 30,
+                color: Colors.green,
+              ),
+            ),
+          ],
+        ),
+        Spacer(
+          flex: 1,
+        ),
+        ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Icon(Icons.arrow_back))
+      ],
+    );
+  }
+}
+
+class TestInputRoute extends StatelessWidget {
+  const TestInputRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  autofocus: false,
+                  decoration: InputDecoration(
+                      labelText: "用户名",
+                      hintText: "用户名或邮箱",
+                      prefixIcon: Icon(Icons.person)),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: "密码",
+                      hintText: "您的登录密码",
+                      prefixIcon: Icon(Icons.lock_clock)),
+                  obscureText: true,
+                ),
+              ],
+            ),
+          ),
+          Text("this is new page"),
+          Container(
+            height: 10,
+          ),
+          ElevatedButton.icon(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            label: Text("Back"),
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.red,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
 void main() {
   runApp(
     MaterialApp(
-      home: Screen(),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => Screen(),
+        "test_input": (context) => TestInputRoute(),
+        "test_layout": (context) => TestLayoutRoute(),
+      },
     ),
   );
 }
