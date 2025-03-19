c1ce7772c88f89cf83315e052fa716d98e12120a
jaysinco <jaysinco@163.com>
2025-03-19 15:57:51 +0800

    add inception block

--- src/deep/common.cpp
+++ src/deep/common.cpp
@@ -1,1 +1,22 @@
 #include "./common.h"
+
+InceptionImpl::InceptionImpl(InceptionOptions const& o)
+{
+    using namespace torch::nn;
+    b1_1_ = register_module("b1_1", Conv2d(Conv2dOptions(o.c0(), o.c1(), 1)));
+    b2_1_ = register_module("b2_1", Conv2d(Conv2dOptions(o.c0(), o.c2()[0], 1)));
+    b2_2_ = register_module("b2_2", Conv2d(Conv2dOptions(o.c2()[0], o.c2()[1], 3).padding(1)));
+    b3_1_ = register_module("b3_1", Conv2d(Conv2dOptions(o.c0(), o.c3()[0], 1)));
+    b3_2_ = register_module("b3_2", Conv2d(Conv2dOptions(o.c3()[0], o.c3()[1], 5).padding(2)));
+    b4_1_ = register_module("b4_1", MaxPool2d(MaxPool2dOptions(3).stride(1).padding(1)));
+    b4_2_ = register_module("b4_2", Conv2d(Conv2dOptions(o.c0(), o.c4(), 1)));
+}
+
+torch::Tensor InceptionImpl::forward(torch::Tensor x)
+{
+    auto b1 = torch::relu(b1_1_(x));
+    auto b2 = torch::relu(b2_2_(torch::relu(b2_1_(x))));
+    auto b3 = torch::relu(b3_2_(torch::relu(b3_1_(x))));
+    auto b4 = torch::relu(b4_2_(b4_1_(x)));
+    return torch::cat({b1, b2, b3, b4}, 1);
+}
--- src/deep/common.h
+++ src/deep/common.h
@@ -4,3 +4,40 @@
 #include "toolkit/args.h"
 #include "toolkit/toolkit.h"
 #include "fwd.h"
+
+template <size_t D>
+using IntArray = std::array<int64_t, D>;
+
+struct InceptionOptions
+{
+    InceptionOptions(int64_t c0, int64_t c1, IntArray<2> c2, IntArray<2> c3, int64_t c4)
+        : c0_(c0), c1_(c1), c2_(c2), c3_(c3), c4_(c4)
+    {
+    }
+
+    // c0 is the number of input channels
+    TORCH_ARG(int64_t, c0);
+
+    // c1-c4 are the number of output channels for each branch
+    TORCH_ARG(int64_t, c1);
+    TORCH_ARG(IntArray<2>, c2);
+    TORCH_ARG(IntArray<2>, c3);
+    TORCH_ARG(int64_t, c4);
+};
+
+struct InceptionImpl: torch::nn::Module
+{
+    explicit InceptionImpl(InceptionOptions const& o);
+    torch::Tensor forward(torch::Tensor x);
+
+private:
+    torch::nn::Conv2d b1_1_ = nullptr;
+    torch::nn::Conv2d b2_1_ = nullptr;
+    torch::nn::Conv2d b2_2_ = nullptr;
+    torch::nn::Conv2d b3_1_ = nullptr;
+    torch::nn::Conv2d b3_2_ = nullptr;
+    torch::nn::MaxPool2d b4_1_ = nullptr;
+    torch::nn::Conv2d b4_2_ = nullptr;
+};
+
+TORCH_MODULE(Inception);
